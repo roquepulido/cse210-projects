ce79e3af384ab6f76a64ce42762bcdc23b357c1f
Roque Alberto Pulido Morales <rapulidom@desarrollo-ultrasist.com.mx>
2023-10-23 17:23:01 -0600

    Add prep 5 assignament

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,32 @@
 using System;
 
 class Program
-{
+{   
+    static string name;
+    static int favoriteNumber;
+    static void DisplayWelcome(){
+        Console.WriteLine("Welcome to the program!");
+    }
+    static void PromptUserName(){
+         Console.WriteLine("Please enter your name: ");
+         name = Console.ReadLine();
+    }
+    static void PromptUserNumber (){
+         Console.WriteLine("Please enter your favorite number: ");
+         favoriteNumber = int.Parse(Console.ReadLine());
+    }
+    static int SquareNumber(int number){
+        return number * number;
+    }
+    static void DisplayResult (){
+         Console.WriteLine($"{name}, the square of your number is {SquareNumber(favoriteNumber)}");
+
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        PromptUserName();
+        PromptUserNumber();
+        DisplayResult();
     }
 }
