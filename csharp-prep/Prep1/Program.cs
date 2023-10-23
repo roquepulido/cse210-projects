b821d5b2f701896ca3ac60b82163431b57c7ddcb
Roque Alberto Pulido Morales <rapulidom@desarrollo-ultrasist.com.mx>
2023-10-23 02:32:45 -0600

    Prep1 assignament

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
+        string firstName;
+        string lastName;
+        Console.WriteLine("What is your first name?");
+        firstName = Console.ReadLine();
+        Console.WriteLine("What is your last name?");
+        lastName = Console.ReadLine();
         Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("");
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
